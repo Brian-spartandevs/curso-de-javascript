e81a48ff596993f5ec00172ffd540e72ba670b45
Brian Arrieta <brian@spartandevs.com>
2023-02-09 13:28:45 -0400

    añadi tipos de operadores y modifique el nmbre de la carpeta sintaxis

---
+++ tipos-de-operadores-2/operadores.js
@@ -0,0 +1,75 @@
+/* operadores matematicos */
+
+/* 
+suma/concatenacion (+)
+resta (-)
+multiplicacion (*)
+division (/)
+modulo (%) 
+*/
+
+/* operadores de asignacion */
+
+/* 
+asignacion (=)
+suma y asignacion a += 3(a= a + 3)
+resta y asignacion a -= 3(a= a - 3)
+multiplicacion y asignacion a *= 3(a= a * 3)
+division y asignacion a /= 2(a= a / 2)
+modulo y asignacion a %= 2(a= a % 2)
+ */
+
+/* operadores de incremento/decremento */
+
+/* 
+post-incremento en 1 a++
+pre-incremento en 1 ++a
+post-decremento en 1 a--
+pre-decremento en 1 --a
+ */
+
+/* Operadores matematicos */
+/* let a = 5;
+let b = 2;
+
+let usuario = "Brian"
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b); */
+
+/* esto es una concatenacion */
+/* console.log("Hola " + usuario) */
+
+/* Operadores de incremento/decremento */
+
+/* let a = 5; */
+
+/* console.log(a)
+console.log(a++); */
+/* console.log(a)
+console.log(++a) */
+/* console.log(a) */
+
+/* console.log(a--)
+console.log(a)
+console.log(--a)
+console.log(a) */
+
+/* Operadores de asignacion */
+
+/* let a = 10 */
+
+/* a = a+3; */
+/* a+=3;
+console.log(a);
+a-=3;
+console.log(a);
+a*=3;
+console.log(a);
+a/=3;
+console.log(a);
+a%=3;
+console.log(a) */
+
