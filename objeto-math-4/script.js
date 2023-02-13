3397378ddeaf5774b273a8096196b0931be2b30f
Brian Arrieta <brian@spartandevs.com>
2023-02-13 17:51:15 -0400

    añadi objeto math

---
+++ objeto-math-4/script.js
@@ -0,0 +1,42 @@
+/* objeto math
+
+objeto estatico
+
+tenemos que usar su nombre para utilizarlo */
+
+/* propiedades: */
+
+/* Math.E - Math.PI */
+
+/* Metodos: */
+/* Math.abs(x) -> devuelve el valor absoluto de x */
+/* Math.ceil(x) -> devuelve el entero mas pequeño mayor o igual a un numero */
+/* Math.floor(x) -> devuelve el entero mas grande menor o igual a un numero */
+/* Math.pow(x,y) -> devuelve la potencia de x elevado a y */
+/* Math.random() -> genera un numero pseudoaleatorio entre 0 y 1 */
+/* Math.round(x) -> devuelve el valor de un numero redondeado al entero mas cercano */
+/* Math.sign(x) -> devuelve el signo de la x, que indica si x es positivo, negativo o cero */
+
+console.log(Math.E);
+console.log(Math.PI);
+
+/* let num=5;
+let numnegativo=-5;
+
+console.log(Math.abs(num));
+console.log(Math.abs(numnegativo)); */
+
+/* let num=5.3;
+
+console.log(Math.ceil(num));
+console.log(Math.floor(num)); */
+
+/* console.log(Math.pow(3,3)); */
+
+/* console.log(Math.random()); */
+
+/* console.log(Math.round(5.3)); */
+
+/* console.log(Math.sign(-6)); */
+
+/* console.log(Math.sqrt(2)); */
