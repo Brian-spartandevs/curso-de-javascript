6d5abdbe7bfaec4a10ef47dba8c7ed048a7a7c94
Brian Arrieta <brian@spartandevs.com>
2023-02-15 21:07:44 -0400

    modifique el archivo script de la carpeta arrays introduccion

--- arrays-introduccion-9/script.js
+++ arrays-introduccion-9/script.js
@@ -22,7 +22,6 @@ let numeros = [1,2,3,4,5];// Array con contenido inicial
 
 -los indices empiezan a contar desde 0
 
-
 */
 
 let numeros = [1,2,3,4,5];
@@ -36,5 +35,7 @@ let palabras=['hola', 'estamos', 'en', 'YouTube'];
 console.log(numeros[0]+numeros[1]);
 
 console.log(`La palabra "${palabras[3]}" tiene ${palabras[3].length} letras`);
+
+/* tambien puden contenes booleanos */
 
 let booleans=[true, true, false];
