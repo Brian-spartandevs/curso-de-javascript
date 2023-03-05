67b2c2431d757c877f123795622de5d827095890
Brian Arrieta <brian@spartandevs.com>
2023-03-05 01:41:52 -0400

    añadi insertar clonar y borrar elementos(2) 30

---
+++ Dom-insertar-clonar-borrar-elementos(2)-30/script.js
@@ -0,0 +1,66 @@
+/* 
+Insertar y eliminar elementos II
+
+    parent.insertBefore(newElement, referenceElement) - Insertar un elemento antes del elementos de referencia
+
+    SOPORTE TOTAL
+    parent.insertAdjacentElement('position', element)
+    parent.insertAdjacentHTML('position', HTML)
+    parent.insertAdjacentText('position', text)
+
+    positions:
+        beforebegin - antes de que empiece(hermano anterior)
+        afterbegin - despues de que empieze (pimer hijo)
+        beforeend - antes de que acabe (ultimo hijo)
+        afterend - despues de que acabe (hermano siguiente)
+
+    parent.replaceChild(newChild, oldChild) - reemplaza un hijo por otro
+*/
+
+const list = document.getElementById('list')
+const newElement = document.createElement('li')
+newElement.textContent = "I'm a new element"
+
+/* list.insertBefore(newElement, list.children[0]) */
+
+/* list.children[0].insertAdjacentElement('beforebegin', newElement) */
+/* list.insertAdjacentElement('afterbegin', newElement) */
+/* list.insertAdjacentElement('beforeend', newElement) */
+/* list.children[1].insertAdjacentElement('afterend', newElement) */
+
+/* list.children[1].insertAdjacentHTML('afterend', '<li>Elemento HTML</li>')
+list.children[1].insertAdjacentText('afterend', '<li>Elemento HTML</li>') */
+
+/* list.replaceChild(newElement, list.children[0]) */
+
+/*
+    DOM manipulation convenience methods - JQuery Like
+    https://caniuse.com/#search=jQuery-like
+    positions:
+        parent.before() - Antes de que empiece (hermano anterior)
+        parent.prepend() - después de que empiece (primer hijo)
+        parent.append() - antes de que acabe (último hijo)
+        parent.after() - después de que acabe (hermano siguiente) 
+        child.replaceWith(newChild)
+*/
+
+/* list.children[0].before(newElement) */
+/* list.prepend(newElement) */
+/* list.append(newElement) */
+/* list.children[1].after(newElement) */
+
+/* list.children[0].replaceWith(newElement) */
+/* document.getElementById('child-to-replace').replaceWith(newElement) */
+
+/*
+    Clonar y eliminar elementos
+        element.cloneNode(true|false) - Clona el nodo. Si le pasamos true clona todo el elemento con los hijos, si le pasamos false clona solo el elemento sin hijos
+        element.remove() - Elimina el nodo del DOM
+        element.removeChild(child) - Elimina el nodo hijo del DOM
+*/
+
+/* list.after(list.cloneNode(true)) */
+
+/* list.remove() */
+
+list.removeChild(list.children[1])
