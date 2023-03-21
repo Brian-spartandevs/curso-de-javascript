0293b2b1e6b6172242ce16223b44eec573c1e8d1
Brian Arrieta <brian@spartandevs.com>
2023-03-20 20:15:17 -0400

    añadi expresiones regulares(I) 44

---
+++ Expresiones-Regulares(1)-Introduccion-44/script.js
@@ -0,0 +1,15 @@
+/* 
+    Expresiones Regulares:
+        Son una secuencia de caracteres que forman un patron de busqueda, principalmente utilizada para la busqueda de patrones de cadenas de caractres u operaciones de sustituciones.
+
+    Sintaxis:
+        /Patron/
+    Banderas:
+        i: Ignore case. No diferencia entre mayusculas y minusculas
+        g: Global. Busca de forma global, es decir, no se para despues de la primera coincidencia
+*/
+
+const text = document.getElementById('text').textContent
+const regEx = /lorem/gi
+
+console.log(regEx.test(text))
